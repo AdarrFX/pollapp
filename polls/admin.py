6dda150f6c7a13b82e7188f56bdc19247016ca3f
AdarrFX <grenador@gmail.com>
2025-03-18 16:26:50 -0400

    Got the voting working.

--- polls/admin.py
+++ polls/admin.py
@@ -2,5 +2,13 @@ from django.contrib import admin
 from .models import Poll
 from .models import Option
 
-admin.site.register(Poll)
-admin.site.register(Option)
+# admin.site.register(Poll)
+# admin.site.register(Option)
+
+class OptionInline(admin.TabularInline):  # Use TabularInline for a table-like display
+    model = Option
+    extra = 0  # Number of empty fields to display for new related objects
+
+@admin.register(Poll)
+class PollAdmin(admin.ModelAdmin):
+    inlines = [OptionInline]  # Attach the inline model to the Poll admin
--- polls/views.py
+++ polls/views.py
@@ -1,16 +1,26 @@
-from django.shortcuts import render, redirect
-from .models import Poll
+from django.shortcuts import render, redirect, get_object_or_404, HttpResponse
+from .models import Poll, Option
 
 def index(request):
     activepolls = Poll.objects.all()
-    return render(request, 'polls/index.html', { 'activepolls': activepolls })
+    return render(request, 'polls/index.html', { 'polls': activepolls })
 
 def about(request):
     return render(request, 'polls/about.html')
 
-def poll(request):
+def active_polls(request):
+    active_polls = Poll.objects.all()
     return render(request, 'polls/poll.html')
 
 def voteOnOption(request):
     if request.method == 'POST':
-        return redirect('/')
+        option_id = request.POST.get('option_id')
+        poll_id = request.POST.get('poll_id')
+        optionToVoteOn = get_object_or_404(Option, id=option_id)
+        pollContainingOption = get_object_or_404(Poll, id=poll_id)
+
+        # Add a vote
+        optionToVoteOn.votes += 1
+        optionToVoteOn.save()
+
+        return HttpResponse(f"Poll updated: {pollContainingOption.question}. Voted for option {optionToVoteOn.option_text}")
